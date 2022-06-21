3ecd84d87855c20d5c1e168134a734410be60e16
alexsantee <alexsantee@hotmail.com>
2022-06-20 22:19:44 -0300

    better segmentation and added evaluation

--- extractor.py
+++ extractor.py
@@ -8,13 +8,15 @@ from scipy import ndimage
 filename = "54HC32_RCA_8825_die_120nmpp_4xsmaller.jpg"
 MEDIAN_RADIUS = 12
 VALUE_THRESHOLD = 220
+SAT_THRESHOLD = 0.05
+CLOSING_SIZE = 5
 
 img = imageio.imread(filename)
 X, Y, _ = img.shape
-img = img[:X//2,:Y//2] # for testing uses one quarter of the image for more performance
+#img = img[:X//2,:Y//2] # for testing uses one quarter of the image for more performance
 img_hsv = mpl.colors.rgb_to_hsv(img)
 
-hist, bins = np.histogram(img_hsv[:,:,2], bins="auto")
+hist, bins = np.histogram(img_hsv[:,:,1], bins="auto")
 plt.plot(bins[:-1], hist)
 plt.show()
 
@@ -25,26 +27,54 @@ plt.show()
 # traces of the layer, so a not blurred threshold is OR'd to this median
 
 # Metal layer is mostly white, so it has a large value
+img_sat = img_hsv[:,:,1]
 img_val = img_hsv[:,:,2]
 # Median filter gets rid of dust particles, but loses resolution
+median_img_sat = ndimage.median_filter(img_sat, size=MEDIAN_RADIUS)
 median_img_val = ndimage.median_filter(img_val, size=MEDIAN_RADIUS)
 
 # uses threshold to extract metal layer
 size = img_val.shape[:2]
-v_thres = (img_val>VALUE_THRESHOLD)*np.ones(size, dtype="bool")
-median_v_thres = (median_img_val>VALUE_THRESHOLD)*np.ones(size, dtype="bool")
-thres_extraction = median_v_thres|v_thres
+v_thres     = (img_val>VALUE_THRESHOLD)*np.ones(size, dtype="bool")
+s_thres     = (img_sat<  SAT_THRESHOLD)*np.ones(size, dtype="bool")
+v_med_thres = (median_img_val>VALUE_THRESHOLD)*np.ones(size, dtype="bool")
+#s_med_thres = (median_img_sat<  SAT_THRESHOLD)*np.ones(size, dtype="bool")
 
-plt.imshow(v_thres, cmap="gray"); plt.title("value threshold")
+metal = (v_thres & s_thres) | v_med_thres# & s_med_thres)
+
+plt.imshow(v_thres, cmap="gray"); plt.title("Value threshold")
 plt.show()
-plt.imshow(median_v_thres, cmap="gray"); plt.title("median filter value threshold")
+plt.imshow(s_thres, cmap="gray"); plt.title("Saturation threshold")
 plt.show()
-plt.imshow(thres_extraction, cmap="gray"); plt.title("OR'd threshold")
+plt.imshow(v_med_thres, cmap="gray"); plt.title("Median blur value threshold")
+plt.show()
+plt.imshow(metal, cmap="gray"); plt.title("Final metal layer")
 plt.show()
 
 # The next step uses morphology operators to get a better segmentation
 
 # opening elliminates too small particles
-img_opening = morphology.binary_opening(thres_extraction, morphology.disk(2))
-plt.imshow(img_opening, cmap="gray"); plt.title("After opening")
+img_closing = morphology.binary_closing(metal, morphology.disk(CLOSING_SIZE))
+plt.imshow(img_closing, cmap="gray"); plt.title("After closing")
 plt.show()
+
+# comparison of closing with manual translation
+ref_filename = "manual_segmentation/54HC32_metal_4xsmaller.gif"
+ref_img = imageio.imread(ref_filename)
+ref_metal = (ref_img[:,:,0] > 0)
+
+plt.imshow(ref_metal, cmap="gray")
+plt.show()
+
+tp = np.sum(img_closing & ref_metal)/(X*Y)
+tn = np.sum(np.logical_not(img_closing) & np.logical_not(ref_metal))/(X*Y)
+fp = np.sum(img_closing & np.logical_not(ref_metal))/(X*Y)
+fn = np.sum(np.logical_not(img_closing) & ref_metal)/(X*Y)
+
+print("True positive:",  tp)
+print("True negative:",  tn)
+print("Correct ratio:", tp+tn)
+print("False positive:", fp)
+print("False negative:", fn)
+print("Incorrect ratio:", fp+fn)
+
